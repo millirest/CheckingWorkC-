627638d8c60b64ee5471933a165bbba779bbb94e
millirest <millirest@mail.ru>
2024-05-25 14:45:07 +0500

    Создание ввода данных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+ длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+ либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+ лучше обойтись исключительно массивами.
+ */
+
+System.Console.Write("Введите колличество элементов массива:");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите строки:");
+string[] Array = new string [M];
+for (int i = 0; i < M; i++)
+{
+    System.Console.Write("Строка:"+i);
+    Array[i] = Console.ReadLine()+"";
+}
+
