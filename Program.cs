be310f4f190b0326498c8eded40d9e213c90fda9
millirest <millirest@mail.ru>
2024-05-25 15:06:03 +0500

    Создание метода создания второго массива

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
-﻿
-
-
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
  длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
  лучше обойтись исключительно массивами.
  */
+
+using System.Linq.Expressions;
 
 System.Console.Write("Введите колличество элементов массива:");
 int M = Convert.ToInt32(Console.ReadLine());
@@ -22,3 +16,21 @@ for (int i = 0; i < M; i++)
     Array[i] = Console.ReadLine()+"";
 }
 
+
+
+void NewArray (string[] Array)
+{
+    int B=0;
+    for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i].Length > 3)
+            B++;
+        }
+    string[] NewArray = new string [B];
+
+    for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i].Length > 3)
+            NewArray[i] = Array[i];
+        }
+}
